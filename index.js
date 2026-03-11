79ef894001a9a52896cfb05fdbe5f787aec3374d
Eduardo Gabriel Kenzo Tanaka <eduduardogabriel888@gmail.com>
2026-03-11 10:41:00 -0300

    add book functionality

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ import moment from "moment";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 const db = new pg.Client({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     host: process.env.PG_HOST,
     database: process.env.PG_DB,
 });
+
 db.connect();
 
 let books = [
@@ -71,7 +73,54 @@ app.get("/add", (req, res) => {
     res.render("add.ejs");
 });
 
-app.post("/add", (req, res) => {});
+app.post("/add", async (req, res) => {
+    const book = {
+        isbn: req.body.isbn || "0000000000",
+        title: req.body.title || "No title",
+        author: req.body.author || "No author",
+        rating: req.body.rating || 0,
+        readIn: req.body.readin || null,
+        synopsis: req.body.synopsis || "",
+        review: req.body.review || "",
+        notes: req.body.notes || "",
+        coverUrl: `https://example.com/${req.body.isbn}`,
+    };
+
+    try {
+        let query = `
+        INSERT INTO books 
+        (isbn, title, author, rating, read_in, synopsis, review, cover_url)
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8) 
+        RETURNING *`;
+
+        let values = [
+            book.isbn,
+            book.title,
+            book.author,
+            book.rating,
+            book.readIn,
+            book.synopsis,
+            book.review,
+            book.coverUrl,
+        ];
+        console.log(values);
+
+        let response = await db.query(query, values);
+        console.log(response);
+        const bookId = response.rows[0].id;
+
+        if (book.notes) {
+            const notesQuery = `INSERT INTO notes (book_id, content) VALUES ($1, $2) RETURNING *`;
+            response = await db.query(notesQuery, [bookId, book.notes]);
+            console.log(response);
+        }
+
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Erro ao inserir livro1");
+    }
+});
 
 app.get("/books/:id/edit", (req, res) => {
     const book = books.find((book) => book.id == req.params.id);
