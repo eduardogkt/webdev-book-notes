518dd1b8343d19c01c88dc4bcb4f63d568f67a71
Eduardo Gabriel Kenzo Tanaka <eduduardogabriel888@gmail.com>
2026-03-11 12:29:23 -0300

    add link to reference site

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const db = new pg.Client({
+const db = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
